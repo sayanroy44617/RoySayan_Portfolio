7af2edd7f9a398251114592a459dcddbb0a1a437
sayanroy44617 <sayanroy44617@gmail.com>
2024-09-17 16:10:13 +0530

    Add. Navbar Component

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -6,6 +6,7 @@ const Projects = () => {
     <div className="h-[60vh] md:w-[80vw] relative">
       <div className="absolute left-[5%] md:left-[10%] lg:left-[28vw]">
         <SectionHeader title="3. Projects I have Worked On ......" />
+        
       </div>
     </div>
   );
